3910015a938883d50b0749749847498c2c6fd8dc
BayCom GmbH <software@baycom.de>
2015-08-22 09:42:11 +0200

    minor cleanup

--- actuators/dimmer/out8-dimmer-bim112/src/channel.cpp
+++ actuators/dimmer/out8-dimmer-bim112/src/channel.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015 Martin Glueck <martin@mangari.org>
+ *  Copyright (c) 2015 Deti Fliegl <deti@fliegl.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 3 as
